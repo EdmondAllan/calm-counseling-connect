a4ab9bea6452d000540ec1cc2e4e96eccc4a47ae
EdmondAllan <231501044@rajalakshmi.edu.in>
2025-08-07 00:07:19 +0530

    dist

--- api/test.ts
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    return res.status(200).json({ message: 'POST received' });
-  }
-  return res.status(405).json({ message: 'Method Not Allowed' });
-} 
