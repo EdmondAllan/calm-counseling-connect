a6250a6bccf9698d5d55dfaeff882fff1594d7e1
EdmondAllan <231501044@rajalakshmi.edu.in>
2025-08-08 23:54:07 +0530

    update9

--- api/create-order.ts
+++ api/create-order.ts
@@ -1,6 +1,9 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 const Razorpay = require('razorpay');
 
+export const config = {
+  runtime: 'nodejs18.x'
+};
 // Debug env variables
 console.log('Environment check:', {
   hasKeyId: !!process.env.RAZORPAY_KEY_ID,
